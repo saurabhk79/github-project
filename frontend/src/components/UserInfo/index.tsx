9c7d5a30db99b39c4f8f7d89a78506a97d3234d3
Saurabh <imsauby@gmail.com>
2024-04-20 13:03:32 +0530

    basic user info page

--- frontend/src/components/UserInfo/index.tsx
+++ frontend/src/components/UserInfo/index.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { config } from "../../config";
 import { UserInterface } from "./user.interface";
+import { RepoInterface } from "./repo.interface";
 
 const UserInfo: React.FC = () => {
     const [userData, setUserData] = useState<UserInterface | null>(null);
+    const [repoData, setRepoData] = useState<RepoInterface[] | []>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const params = useParams();
 
@@ -13,16 +15,19 @@ const UserInfo: React.FC = () => {
         const data = await axios.get(
             config.base_url + "/user/save-user/" + params.user
         );
-        console.log(data);
+        const repoData = await axios.get(data.data.repos_url);
 
         setUserData(data.data);
+        setRepoData(repoData.data);
     };
+
     useEffect(() => {
         console.log(params.user);
         setLoading(true);
         getSetUser();
         setLoading(false);
     }, [params]);
+
     return (
         <div>
             {loading ? (
@@ -39,7 +44,25 @@ const UserInfo: React.FC = () => {
                                 <h1>{userData.login}</h1>
                                 <p>{userData.bio}</p>
                             </div>
-                            <div className="grid"></div>
+                            <div className="grid">
+                                {repoData && (
+                                    <>
+                                        {repoData.map((repo: RepoInterface) => {
+                                            return (
+                                                <Link
+                                                    to={
+                                                        "/repo/" +
+                                                        repo.full_name
+                                                    }
+                                                    key={repo.id}
+                                                >
+                                                    {repo.name}
+                                                </Link>
+                                            );
+                                        })}
+                                    </>
+                                )}
+                            </div>
                         </div>
                     )}
                 </>
---
+++ frontend/src/components/UserInfo/repo.interface.ts
@@ -0,0 +1,10 @@
+export interface RepoInterface {
+    id: string;
+    name: string;
+    full_name: string;
+    size: number;
+    language: string;
+    visibility: string;
+    description: string;
+    default_branch: string;
+}
