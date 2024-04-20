00c0df00ac8cbf266d024969a90033e8bb1b04c3
Sauby <imsauby@gmail.com>
2024-04-20 21:50:37 +0530

    added styles for followers

--- frontend/src/components/Followers/index.tsx
+++ frontend/src/components/Followers/index.tsx
@@ -2,53 +2,49 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 import { UserInterface } from "../UserInfo/user.interface";
+import styles from "./followers.module.css";
 
 const Followers: React.FC = () => {
-    const [followersData, setFollowersData] = useState<UserInterface[] | []>(
-        []
+  const [followersData, setFollowersData] = useState<UserInterface[] | []>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const params = useParams();
+
+  const getSetRepo = async () => {
+    const followers = await axios.get(
+      "https://api.github.com/users/" + params.user + "/followers"
     );
-    const [loading, setLoading] = useState<boolean>(true);
-    const params = useParams();
+    console.log(followers);
 
-    const getSetRepo = async () => {
-        const followers = await axios.get(
-            "https://api.github.com/users/" + params.user + "/followers"
-        );
-        console.log(followers);
+    setFollowersData(followers.data);
+  };
 
-        setFollowersData(followers.data);
-    };
-
-    useEffect(() => {
-        setLoading(true);
-        getSetRepo();
-        setLoading(false);
-    }, [params]);
-    return (
-        <div>
-            {loading ? (
-                <>Loading...</>
-            ) : (
-                <>
-                    {followersData && (
-                        <>
-                            {followersData.map((follower) => {
-                                return (
-                                    <Link to={`/${follower.login}`}>
-                                        <img
-                                            src={follower.avatar_url}
-                                            alt={follower.login}
-                                        />
-                                        <em>{follower.login}</em>
-                                    </Link>
-                                );
-                            })}
-                        </>
-                    )}
-                </>
-            )}
-        </div>
-    );
+  useEffect(() => {
+    setLoading(true);
+    getSetRepo();
+    setLoading(false);
+  }, [params]);
+  return (
+    <div className={styles.follower}>
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        <>
+          {followersData && (
+            <>
+              {followersData.map((follower) => {
+                return (
+                  <Link to={`/${follower.login}`}>
+                    <img src={follower.avatar_url} alt={follower.login} />
+                    <em>{follower.login}</em>
+                  </Link>
+                );
+              })}
+            </>
+          )}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Followers;
