033ebaeb7e5f7e587dcc029da69053e7779b77cf
Saurabh <imsauby@gmail.com>
2024-04-20 11:26:16 +0530

    created

--- frontend/src/components/UserInfo/index.tsx
+++ frontend/src/components/UserInfo/index.tsx
@@ -2,34 +2,50 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { config } from "../../config";
+import { UserInterface } from "./user.interface";
 
 const UserInfo: React.FC = () => {
-  const [userData, setUserData] = useState({});
-  const [loading, setLoading] = useState<boolean>(false);
-  const params = useParams();
+    const [userData, setUserData] = useState<UserInterface | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const params = useParams();
 
-  const getSetUser = async () => {
-    const data = await axios.get(config.base_url + "/user/save-user/" + params.user);
-    console.log(data);
-  }
-  useEffect(() => {
-    console.log(params.user);
+    const getSetUser = async () => {
+        const data = await axios.get(
+            config.base_url + "/user/save-user/" + params.user
+        );
+        console.log(data);
 
-    getSetUser();
-
-  }, [params]);
-  return (
-    <div>
-      {loading ? (
-        <>Loading...</>
-      ) : (
-        <>
-          <div></div>
-          <div className="grid"></div>
-        </>
-      )}
-    </div>
-  );
+        setUserData(data.data);
+    };
+    useEffect(() => {
+        console.log(params.user);
+        setLoading(true);
+        getSetUser();
+        setLoading(false);
+    }, [params]);
+    return (
+        <div>
+            {loading ? (
+                <>Loading...</>
+            ) : (
+                <>
+                    {userData && (
+                        <div>
+                            <div>
+                                <img
+                                    src={userData.avatar_url}
+                                    alt={userData.login}
+                                />
+                                <h1>{userData.login}</h1>
+                                <p>{userData.bio}</p>
+                            </div>
+                            <div className="grid"></div>
+                        </div>
+                    )}
+                </>
+            )}
+        </div>
+    );
 };
 
 export default UserInfo;
---
+++ frontend/src/components/UserInfo/user.interface.ts
@@ -0,0 +1,15 @@
+export interface UserInterface {
+    login: string;
+    id: string;
+    avatar_url?: string;
+    type?: string;
+    name?: string;
+    company?: string;
+    blog?: string;
+    location?: string;
+    email?: string;
+    bio?: string;
+    public_repos?: number;
+    followers?: string;
+    following?: string;
+}
